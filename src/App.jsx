41d9c0925c3a8a2bf17aa19b875b76140919a605
K-S11145T <tanushsingh843@gmail.com>
2025-03-09 11:30:26 +0530

    Portfolio-Website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GridBackgroundDemo } from "./components/Background/Background";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center overflow-hidden">
+      <GridBackgroundDemo />
+    </div>
+  )
+}
+
+export default App
